cb37bb6c769ad4dadad117cad19ecead237799be
Equinox216 <gabmontanez@pm.me>
2026-03-06 13:16:02 -0400

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tactics-lab/sw.js', { scope: '/tactics-lab/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tactic-lab/sw.js', { scope: '/tactic-lab/' })})}
